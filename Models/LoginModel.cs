17653cd14d6eba5b19b4ccdcc544d00a1ffdc304
DimiturDj <167714606+DimiturDj@users.noreply.github.com>
2025-04-06 14:07:23 +0300

    Create LoginModel.cs

---
+++ Models/LoginModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RepairAndConstruction.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Username is required.")]
+        [StringLength(50, ErrorMessage = "Username cannot be longer than 50 characters.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(100, ErrorMessage = "Password cannot be longer than 100 characters.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string Role { get; set; } // Added Role field to the model
+    }
+}
